72642f25fe3bbf9faac5042b6a0925a444ec02df
semenov-k <k.semenov@indevstudio.com>
2024-05-07 17:44:04 +0300

    persist session context

--- src/main/java/com/study/backend/config/QuartzConfig.java
+++ src/main/java/com/study/backend/config/QuartzConfig.java
@@ -1,5 +1,8 @@
 package com.study.backend.config;
 
+import com.study.backend.quartz.AutowiringSpringBeanJobFactory;
+import org.quartz.Scheduler;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.quartz.SchedulerFactoryBean;
@@ -17,6 +20,22 @@ public class QuartzConfig {
         schedulerFactory.setSchedulerName("test");
 
         return schedulerFactory;
+    }
+
+    @Bean
+    public SchedulerFactoryBean schedulerFactory(ApplicationContext applicationContext) {
+        SchedulerFactoryBean factoryBean = new SchedulerFactoryBean();
+        AutowiringSpringBeanJobFactory jobFactory = new AutowiringSpringBeanJobFactory();
+        jobFactory.setApplicationContext(applicationContext);
+
+        factoryBean.setJobFactory(jobFactory);    // Set jobFactory to AutowiringSpringBeanJobFactory
+        return factoryBean;
+    }
+
+    @Bean
+    public Scheduler getScheduller(SchedulerFactoryBean factoryBean){
+        return factoryBean.getScheduler();
+
     }
 
 }
--- src/main/java/com/study/backend/quartz/EmployeeQueryJob.java
+++ src/main/java/com/study/backend/quartz/EmployeeQueryJob.java
@@ -1,18 +1,33 @@
 package com.study.backend.quartz;
 
+import com.study.backend.enity.Employee;
+import com.study.backend.repository.EmployeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.quartz.PersistJobDataAfterExecution;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 
 @Slf4j
 @DisallowConcurrentExecution
+@PersistJobDataAfterExecution
+@Component
 public class EmployeeQueryJob extends QuartzJobBean {
+    @Autowired
+    EmployeeRepository employeeRepository;
+
 
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         log.info("Job RUN");
+
+        List<Employee> employeeList = this.employeeRepository.getEmployees();
+        employeeList.stream().map(Employee::toString).forEach(e -> log.info("\033[1;32m Value:\033[0m \033[0;36m{}\033[0m", e));
     }
 }
