9fb79af4d64f4a9ca29e9c1e31258c58cc5f0189
semenov-k <k.semenov@indevstudio.com>
2024-05-14 15:12:38 +0300

    persist session context

--- src/main/java/com/study/backend/quartz/TestJob.java
+++ src/main/java/com/study/backend/quartz/TestJob.java
@@ -1,14 +1,20 @@
 package com.study.backend.quartz;
 
+import com.study.backend.repository.EmployeeRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.quartz.DisallowConcurrentExecution;
 import org.quartz.JobExecutionContext;
 import org.quartz.JobExecutionException;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.quartz.QuartzJobBean;
 
 @DisallowConcurrentExecution
 @Slf4j
 public class TestJob extends QuartzJobBean {
+
+    @Autowired
+    EmployeeRepository employeeRepository;
+
     @Override
     protected void executeInternal(JobExecutionContext context) throws JobExecutionException {
         log.info("Job is running");
